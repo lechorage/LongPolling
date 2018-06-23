c3597f1cefb7c7d8212413d56f68c09cc800bc44
Yuan Sun <yus74@pitt.edu>
2018-06-23 22:50:22 +0800

    更新

--- TestClient/src/main/java/controller/LongPollingController.java
+++ TestClient/src/main/java/controller/LongPollingController.java
@@ -1,11 +1,37 @@
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import dto.PushMsg;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @RestController
 @RequestMapping("/longpolling/")
 public class LongPollingController {
+    private static final Type notificationsTypeReference =
+            new TypeToken<List<PushMsg>>() {
+            }.getType();
+    @Autowired
+    private Gson gson;
+
+    public DeferredResult<PushMsg> getMessage(
+        @RequestParam(value = "userId") String userId,
+        @RequestParam(value = "notifications") String notificationsAsString){
+        List<PushMsg> msgs = null;
+        try{
+            msgs = gson.fromJson(notificationsAsString,notificationsTypeReference);
+        }catch (Throwable ex){
+            ex.printStackTrace();
+        }
+
+    }
 
 
 }
